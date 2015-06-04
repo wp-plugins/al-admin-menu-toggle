9167234083fab0cb89fc180a63ee6989a5b7031a
sudhakarc410 <sudhakarc410@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-04 11:48:03 +0000

    1.1 Update
    git-svn-id: https://plugins.svn.wordpress.org/al-admin-menu-toggle/tags/1.0.1@1174361 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin-menu-toggle.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: AL | Admin Menu Toggle
+Plugin URI: https://wordpress.org/plugins/al-admin-menu-toggle/
+Description: 	Creates Toggle Menu for WordPress Admin Menu in the Sidebar.   Easy to Install and Easy to Use!!!
+Author: Sudhakar. C
+Author URI: https://profiles.wordpress.org/sudhakarc410/
+Version: 1.0.0
+*/
+
+// +----------------------------------------------------------------------+
+// | Copyright Team AL (#)                                                |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License, version 2, as  |
+// | published by the Free Software Foundation.                           |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to the Free Software          |
+// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,               |
+// | MA 02110-1301 USA                                                    |
+// +----------------------------------------------------------------------+
+
+add_action( 'admin_head','amt_custom_menu_class' );
+
+function amt_custom_menu_class() {
+    global $menu;
+    foreach( $menu as $key => $value ) {
+        $menu[$key][4] .= " amt-custom-class";
+    }
+	?>
+	<style type="text/css">
+		<!--
+		.wp-submenu.wp-submenu-wrap {
+			left: auto;
+			position: relative;
+			top: auto;
+		}
+		#adminmenu .amt-custom-class .wp-submenu {
+			left: auto;
+			position: relative;
+			top: auto;
+		}	
+		#adminmenu .wp-has-current-submenu .wp-submenu {
+			left: auto;
+			position: relative;
+			top: auto;
+			display: block;
+		}
+		#adminmenumain {
+			clear: none;
+			float: left;
+			overflow: hidden;
+		}
+		body {
+			height: auto;
+		}
+		.sticky-menu #adminmenuwrap {
+			position: relative;
+		}
+		-->		
+	</style>
+	<script>	
+		<!--
+		(function($) {	
+			$( document ).ready(function() {
+				console.log( "Toggle Menu Working!" );
+				$('.wp-submenu').hide();
+				$('.wp-has-current-submenu .wp-submenu').show();
+				$('.wp-has-submenu a.amt-custom-class').click(function(e) {
+					e.preventDefault();
+					$(this).parent().find(".wp-submenu").slideToggle( "slow" );
+					//$(this).parent().find(".wp-submenu").toggleClass("amt-toggle-active");
+				});	
+			});			
+		})( jQuery );
+		-->	
+	</script>
+	<?php
+}
